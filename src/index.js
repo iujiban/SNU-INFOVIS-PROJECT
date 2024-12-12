316709e6bea0d5ccf5c668480f315251e2faf89e
Jaeyong <study@snu.ac.kr>
2024-12-13 01:03:38 +0900

    install font awesome

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import 'antd/dist/antd.css';
 import Dashboard from './Dashboard';
+import "./fontawesome";
 
 ReactDOM.render(<Dashboard />, document.getElementById('root'));
